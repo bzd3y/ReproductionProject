bc0cc0cf1c55d35c2e4ef3d8c864a3dea964e00c
Davidson, Benjamin Zachary (bzd3y) <bzd3y@virginia.edu>
2024-02-22 11:42:12 -0500

    Add project files.

---
+++ ReproductionProject/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ReproductionProject
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate
+	{
+		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+	}
+}
