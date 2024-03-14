2a20c81e9f268e3c98eb725443b2dd446c2c66d1
Davidson, Benjamin Zachary (bzd3y) <bzd3y@virginia.edu>
2024-03-14 18:21:57 -0400

    added Focused project

---
+++ Focused/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Focused
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new AppShell();
+		}
+	}
+}
---
+++ Focused/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿namespace Focused
+{
+	public partial class MainPage : ContentPage
+	{
+		int count = 0;
+
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		private void OnCounterClicked(object sender, EventArgs e)
+		{
+			count++;
+
+			if (count == 1)
+				CounterBtn.Text = $"Clicked {count} time";
+			else
+				CounterBtn.Text = $"Clicked {count} times";
+
+			SemanticScreenReader.Announce(CounterBtn.Text);
+		}
+
+		private void TimePicker_Focused(object sender, FocusEventArgs e)
+		{
+
+		}
+
+		private void TimePicker_Unfocused(object sender, FocusEventArgs e)
+		{
+
+		}
+
+		private void DatePicker_Focused(object sender, FocusEventArgs e)
+		{
+
+		}
+
+		private void DatePicker_Unfocused(object sender, FocusEventArgs e)
+		{
+
+		}
+	}
+
+}
