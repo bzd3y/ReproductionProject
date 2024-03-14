2d56ca3a227fa20d50510dd5804f595e33f3ccd2
Davidson, Benjamin Zachary (bzd3y) <bzd3y@virginia.edu>
2024-03-14 17:24:27 -0400

    added ListView project

---
+++ ListView/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿
+namespace ListView
+{
+	public partial class MainPage : ContentPage
+	{
+		int count = 0;
+
+		public MainPage()
+		{
+			InitializeComponent();
+
+			if (OperatingSystem.IsOSPlatform("iOS"))
+			{
+				listView.BackgroundColor = Colors.White;
+			}
+
+			listView.ItemsSource = new List<string>() { "One", "Two", "Three" };
+		}
+
+		private void OnCounterClicked(object sender, EventArgs e)
+		{
+			count++;
+
+			if (count == 1)
+				CounterBtn.Text = $"Clicked {count} time";
+			else
+				CounterBtn.Text = $"Clicked {count} times";
+
+			SemanticScreenReader.Announce(CounterBtn.Text);
+		}
+	}
+
+}
