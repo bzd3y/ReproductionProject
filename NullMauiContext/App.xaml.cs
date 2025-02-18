039f6bb1f85a326dc65c738bb8431e47ba744615
Davidson, Benjamin Zachary (bzd3y) <bzd3y@virginia.edu>
2025-02-18 09:08:11 -0500

    Added NullMauiContext project

---
+++ NullMauiContext/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace NullMauiContext
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+		}
+
+		protected override Window CreateWindow(IActivationState? activationState)
+		{
+			return new Window(activationState?.Context.Services.GetRequiredService<AppShell>()!);
+		}
+	}
+}
---
+++ NullMauiContext/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace NullMauiContext
+{
+	public partial class AppShell : Shell
+	{
+		public AppShell()
+		{
+			InitializeComponent();
+		}
+	}
+}
